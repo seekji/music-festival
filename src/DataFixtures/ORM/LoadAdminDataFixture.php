5ad186eaa2bb3bf93ecec344acdecc845b54ce75
Denis Basov <denibasov@yandex.ru>
2019-10-27 16:15:25 +0300

    task_6 Create admin fixture, add .env for travis.

---
+++ src/DataFixtures/ORM/LoadAdminDataFixture.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use App\Application\Sonata\UserBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class LoadAdminDataFixture
+ * @package App\DataFixtures\ORM
+ */
+class LoadAdminDataFixture extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Sets the container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager.
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $manager->persist(static::getFakeAdmin());
+        $manager->persist(static::getFakeModerator());
+
+        $manager->flush();
+    }
+
+    /**
+     * @return User
+     */
+    public static function getFakeAdmin(): User
+    {
+        $admin = new User();
+
+        $admin->setEmail('admin@symfony.com');
+        $admin->setUsername('admin');
+        $admin->setPlainPassword('admin');
+        $admin->setEnabled(true);
+        $admin->setSuperAdmin(true);
+
+        return $admin;
+    }
+
+    /**
+     * @return User
+     */
+    public static function getFakeModerator(): User
+    {
+        $moderator = (new User())
+            ->setEmail('moderator@symfony.com')
+            ->setUsername('moderator')
+            ->setPlainPassword('moderator')
+            ->setEnabled(true)
+            ->setRoles([
+                'ROLE_ADMIN',
+            ]);
+
+        return $moderator;
+    }
+
+    /**
+     * Get the order of this fixture.
+     *
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
