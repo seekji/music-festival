68511dd6841b358700d8e7d1703e6a6b02b20d3f
Kristina Kovach <Goncharenkokn@gmail.com>
2019-11-10 20:52:02 +0300

    task_16 add menu toggler

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -2,3 +2,4 @@ import 'slick-carousel/slick/slick.js';
 import 'slick-carousel/slick/slick.css';
 
 import backgroundSlider from '../components/background_slider.js';
+import menuToggler from '../components/menu_toggler.js';
