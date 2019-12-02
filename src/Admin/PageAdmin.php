2d162fcd66d028d2d4e99ac2ad09ca14cc13f7bc
Denis Basov <denibasov@yandex.ru>
2019-12-02 23:24:48 +0300

    task_19 Add preview functionality

--- src/Admin/PageAdmin.php
+++ src/Admin/PageAdmin.php
@@ -3,20 +3,20 @@
 namespace App\Admin;
 
 use App\Entity\Locale\LocaleInterface;
+use App\Entity\Page;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Form\Type\ModelListType;
+use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\Form\Type\DateTimePickerType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 /**
  * Class PageAdmin
  * @package App\Admin
  */
-class PageAdmin extends AbstractAdmin
+class PageAdmin extends AbstractAdmin implements PreviewableAdminInterface
 {
     /**
      * @param DatagridMapper $filter
@@ -61,11 +61,11 @@ class PageAdmin extends AbstractAdmin
     {
         $form
             ->with('Свойства страницы', ['class' => 'col-md-9'])
-                ->add('locale', ChoiceType::class, [
-                    'choices' => array_flip(LocaleInterface::LOCALE_LIST)
-                ])
-                ->add('title')
-                ->add('slug')
+            ->add('locale', ChoiceType::class, [
+                'choices' => array_flip(LocaleInterface::LOCALE_LIST)
+            ])
+            ->add('title')
+            ->add('slug')
             ->end()
             ->with('Состояние', ['class' => 'col-md-3'])
             ->end();
@@ -82,5 +82,46 @@ class PageAdmin extends AbstractAdmin
             ->add('slug')
             ->add('createdAt')
             ->add('updatedAt');
+    }
+
+    /**
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->add('preview');
+    }
+
+    /**
+     * @return string
+     */
+    public function getPreviewRouteName(): string
+    {
+        if($this->getLocale() === LocaleInterface::LAN_EN) {
+            return 'app.page.en';
+        }
+
+        return 'app.page';
+    }
+
+    /**
+     * @param $object
+     * @return array
+     */
+    public function getPreviewRouteParameters($object): array
+    {
+        if (!$object instanceof Page) {
+            throw new \LogicException();
+        }
+
+        return ['slug' => $object->getSlug()];
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale(): string
+    {
+        return $this->getSubject()->getLocale();
     }
 }
