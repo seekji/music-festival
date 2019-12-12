01f66267819613674844888f6adc08124e170fe1
Denis Basov <denibasov@yandex.ru>
2019-12-12 23:20:45 +0300

    task_16 create data transformer callback for eventStartDate

--- src/Form/Type/SettingsForm.php
+++ src/Form/Type/SettingsForm.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 
 use Sonata\Form\Type\DatePickerType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -30,9 +31,21 @@ class SettingsForm extends AbstractType
             ->add('en_email', EmailType::class, ['label' => 'Email', 'required' => false]);
 
         $builder
+            ->add('eventStartDate', DatePickerType::class, ['label' => 'Дата начала мероприятия', 'required' => false])
             ->add('vk', TextType::class, ['label' => 'VK', 'required' => false, 'help' => 'Ссылка на группу.'])
             ->add('facebook', TextType::class, ['label' => 'Facebook', 'required' => false, 'help' => 'Ссылка на группу.'])
             ->add('instagram', TextType::class, ['label' => 'Instagram', 'required' => false, 'help' => 'Ссылка на профиль.']);
+
+        $builder->get('eventStartDate')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($date) {
+                    return (new \DateTime($date));
+                },
+                function ($date) {
+                    return  $date->format('Y-m-d');
+                }
+            ))
+        ;
     }
 
     /**
