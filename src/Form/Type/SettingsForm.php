af2598eaaf70eb968e47221bbc5bab9bc41becf2
Denis Basov <denibasov@yandex.ru>
2019-12-18 03:18:22 +0300

    task_16 create static keys and integrate

--- src/Form/Type/SettingsForm.php
+++ src/Form/Type/SettingsForm.php
@@ -5,6 +5,8 @@ namespace App\Form\Type;
 use Sonata\Form\Type\DatePickerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -12,6 +14,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class SettingsForm extends AbstractType
 {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
@@ -19,24 +22,50 @@ class SettingsForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('ru_lineup_description', TextareaType::class, ['label' => 'Лайнап, описание', 'required' => false])
+            ->add('ru_lineup_hours_description', TextareaType::class, ['label' => 'Лайнап, описание (часы)', 'required' => false])
+            ->add('ru_lineup_description', TextareaType::class, ['label' => 'История, описание', 'required' => false])
+            ->add('ru_lineup_link', TextType::class, ['label' => 'Лайнап, ссылка', 'required' => false])
+            ->add('ru_history_description', TextareaType::class, ['label' => 'История, описание', 'required' => false])
+            ->add('ru_history_views_count', TextType::class, ['label' => 'История, кол-во зрителей', 'required' => false])
+            ->add('ru_history_link', TextType::class, ['label' => 'История, ссылка', 'required' => false])
+            ->add('ru_history_punk_link', TextType::class, ['label' => 'История, ссылка (панк)', 'required' => false])
+            ->add('ru_place_title', TextType::class, ['label' => 'Локация, заголовок', 'required' => false])
+            ->add('ru_place_description', TextareaType::class, ['label' => 'Локация, описание', 'required' => false])
+            ->add('ru_place_count', TextType::class, ['label' => 'Локация, кол-во фан-зон', 'required' => false])
+            ->add('ru_place_link', TextType::class, ['label' => 'Локация, ссылка', 'required' => false])
             ->add('ru_address', TextType::class, ['label' => 'Адрес', 'required' => false])
             ->add('ru_phone', TextType::class, ['label' => 'Телефон', 'required' => false])
             ->add('ru_email', EmailType::class, ['label' => 'Email', 'required' => false])
             ->add('ru_email', EmailType::class, ['label' => 'Email', 'required' => false]);
 
         $builder
+            ->add('en_lineup_description', TextareaType::class, ['label' => 'Лайнап, описание', 'required' => false])
+            ->add('en_lineup_hours_description', TextareaType::class, ['label' => 'Лайнап, описание (часы)', 'required' => false])
+            ->add('en_lineup_link', TextType::class, ['label' => 'Лайнап, ссылка', 'required' => false])
+            ->add('en_history_description', TextareaType::class, ['label' => 'История, описание', 'required' => false])
+            ->add('en_history_views_count', TextType::class, ['label' => 'История, кол-во зрителей', 'required' => false])
+            ->add('en_history_link', TextType::class, ['label' => 'История, ссылка', 'required' => false])
+            ->add('en_history_punk_link', TextType::class, ['label' => 'История, ссылка (панк)', 'required' => false])
+            ->add('en_place_title', TextType::class, ['label' => 'Локация, заголовок', 'required' => false])
+            ->add('en_place_description', TextareaType::class, ['label' => 'Локация, описание', 'required' => false])
+            ->add('en_place_count', TextType::class, ['label' => 'Локация, кол-во фан-зон', 'required' => false])
+            ->add('en_place_link', TextType::class, ['label' => 'Локация, ссылка', 'required' => false])
             ->add('en_address', TextType::class, ['label' => 'Адрес', 'required' => false])
             ->add('en_phone', TextType::class, ['label' => 'Телефон', 'required' => false])
             ->add('en_email', EmailType::class, ['label' => 'Email', 'required' => false])
             ->add('en_email', EmailType::class, ['label' => 'Email', 'required' => false]);
 
         $builder
-            ->add('eventStartDate', DatePickerType::class, ['label' => 'Дата начала мероприятия', 'required' => false])
+            ->add('event_start_date', DatePickerType::class, ['label' => 'Дата начала мероприятия', 'required' => false])
+            ->add('event_hours', IntegerType::class, ['label' => 'Длительность мероприятия (часы)', 'required' => false])
+            ->add('tickets_link', TextType::class, ['label' => 'Ссылка на билеты', 'required' => false])
             ->add('vk', TextType::class, ['label' => 'VK', 'required' => false, 'help' => 'Ссылка на группу.'])
             ->add('facebook', TextType::class, ['label' => 'Facebook', 'required' => false, 'help' => 'Ссылка на группу.'])
-            ->add('instagram', TextType::class, ['label' => 'Instagram', 'required' => false, 'help' => 'Ссылка на профиль.']);
+            ->add('instagram', TextType::class, ['label' => 'Instagram', 'required' => false, 'help' => 'Ссылка на профиль.'])
+            ->add('youtube', TextType::class, ['label' => 'YouTube', 'required' => false]);
 
-        $builder->get('eventStartDate')
+        $builder->get('event_start_date')
             ->addModelTransformer(new CallbackTransformer(
                 function ($date) {
                     return (new \DateTime($date));
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,12 +2,21 @@
 
     namespace App\Twig;
 
+    use App\Service\SettingsService;
     use Twig\Extension\AbstractExtension;
     use Twig\TwigFunction;
     use Twig\TwigFilter;
 
     class AppExtension extends AbstractExtension
     {
+
+        private $settingsService;
+
+        public function __construct(SettingsService $settingsService)
+        {
+            $this->settingsService = $settingsService;
+        }
+
         /**
          * @return array|TwigFilter[]
          */
@@ -18,6 +27,14 @@
             ];
         }
 
+        public function getFunctions()
+        {
+            return [
+                new TwigFunction('getSiteSettingByKey', [$this, 'getSiteSettingByKey']),
+                new TwigFunction('getLocalSiteSettingByKey', [$this, 'getLocalSiteSettingByKey']),
+            ];
+        }
+
         /**
          * @param $phone
          * @return string|string[]|null
@@ -25,5 +42,15 @@
         public function phoneHref($phone)
         {
             return preg_replace('/[^0-9]/', '', $phone);
+        }
+
+        public function getSiteSettingByKey(string $key): ?string
+        {
+            return $this->settingsService->getValues()[$key] ?? null;
+        }
+
+        public function getLocalSiteSettingByKey(string $locale, string $key): ?string
+        {
+            return $this->settingsService->getValues()[$locale . '_' . $key] ?? null;
         }
     }
