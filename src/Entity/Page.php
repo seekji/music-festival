ed085a922891e253993ef2ba28b6eadc339432ef
Denis Basov <denibasov@yandex.ru>
2019-12-21 20:21:56 +0300

    task_26 add fields for unique constraint

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PageRepository")
- * @UniqueEntity("slug", message="slug.not_unique")
+ * @UniqueEntity(fields={"slug", "locale"}, message="slug.not_unique")
  */
 class Page
 {
