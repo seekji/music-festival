a21c29274e401f85d591ddfa6432eeb665f5bc28
Kristina Kovach <Goncharenkokn@gmail.com>
2019-12-25 02:48:27 +0300

    task_33 fixes

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -19,6 +19,14 @@ $(document).ready(function () {
 
     // Unable to pass array of elements to plugin
     // Issue: https://github.com/dixonandmoe/rellax/pull/160
+    var headerRellax = new Rellax('.page-header__logo', {
+        speed: 2,
+    });
+
+    var startScreenRellax = new Rellax('.start-screen__title', {
+        speed: 2,
+    });
+
     var promoRellax = new Rellax('.promo__start-timer', {
         speed: 2,
         wrapper: '.promo',
@@ -26,14 +34,14 @@ $(document).ready(function () {
     });
 
     var partnersRellax = Rellax('.partners__title', {
-        speed: 2,
+        speed: 1,
         wrapper: '.page-footer',
         relativeToWrapper: true,
     });
 
     var lineupRellax = new Rellax('#lineup .buy-tickets__link', {
-        speed: 1,
-        wrapper: '#lineup',
+        speed: 0.1,
+        wrapper: '.lineup__buy-tickets',
         relativeToWrapper: true,
     });
 
@@ -61,7 +69,7 @@ $(document).ready(function () {
         historyRellax.refresh();
     };
 
-    if ($(window).width() <= 768 ) {
+    if ($(window).width() <= 1023 ) {
         destroyRellaxEntities();
     } else {
         refreshRellaxEntities()
@@ -69,7 +77,7 @@ $(document).ready(function () {
 
     $(window).resize(function() {
 
-        if ($(window).width() <= 768 ) {
+        if ($(window).width() <= 1023 ) {
             destroyRellaxEntities();
         } else {
             refreshRellaxEntities();
